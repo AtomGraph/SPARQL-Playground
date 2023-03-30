f5d091bdabed10f8f2a8ca0ac61c2f851665c437
Martynas <martynas@atomgraph.com>
2023-03-30 23:33:48 +0200

    Both RDF and SPARQL results rendered as an HTML <table>

--- sparql.js
+++ sparql.js
@@ -5,37 +5,42 @@ window.onload = function() {
 async function query() {
 	const baseURI = window.location.href.split('#')[0];
 	const queryString = document.getElementById("query-string").value.trim();
-	console.log("Query: " + queryString);
-
+	const inputMediaType = document.getElementById("rdf-input-media-type").value;
 	const sources = [
 			{
-			  type: 'stringSource',
-			  value: document.getElementById("rdf-input").value,
-			  mediaType: 'text/turtle',
-			  baseIRI: baseURI,
+				type: 'stringSource',
+				value: document.getElementById("rdf-input").value,
+				mediaType: inputMediaType,
+				baseIRI: baseURI,
 			}
 		];
 
-	if (queryString.startsWith("DESCRIBE") || queryString.startsWith("CONSTRUCT"))
-	{
-		const engine = new Comunica.QueryEngine();
-		const result = await engine.query(queryString, { "sources": sources });
-		console.log(result);
-		const { data } = await engine.resultToString(result, "text/turtle");
-		const resultString = await streamToString(data);
-		document.getElementById("output").value = resultString;
-	}
-	else // SELECT
-	{
-		const engine = new Comunica.QueryEngine();
-		const result = await engine.query(queryString, { "sources": sources });
-		console.log(result);
-		const { data } = await engine.resultToString(result, "application/sparql-results+xml");
-		const resultString = await streamToString(data);
-		document.getElementById("output").value = resultString;
-	}
-	// TO-DO: ASK https://comunica.dev/docs/query/getting_started/query_app/#6--executing-sparql-ask-queries
+	const engine = new Comunica.QueryEngine();
+	var result = await engine.query(queryString, { "sources": sources });
+	const resultsDiv = document.getElementById("results");
+	const outputMediaType = document.getElementById("output-media-type").value;
+	const { data } = await engine.resultToString(result, outputMediaType);
+	const resultString = await streamToString(data);
+	document.getElementById("output").value = resultString;
 
+	// query again to get results in JSON which will be used to render HTML results
+	result = await engine.query(queryString, { "sources": sources });
+	if (result.resultType == "quads")
+	{
+		const { data } = await engine.resultToString(result, "application/ld+json");
+		const jsonString = await streamToString(data);
+		const resultJson = JSON.parse(jsonString);
+
+		renderJSONLD(resultJson, resultsDiv);
+	}
+	if (result.resultType == "bindings")
+	{
+		const { data } = await engine.resultToString(result, "application/sparql-results+json");
+		const jsonString = await streamToString(data);
+		const resultJson = JSON.parse(jsonString);
+
+		renderSPARQLJSON(resultJson, resultsDiv);
+	}
 }
 
 function streamToString(stream) {
@@ -49,4 +54,133 @@ function streamToString(stream) {
 	})
 	stream.on('error', reject)
   })
+}
+
+function renderJSONLD(resultJson, resultsDiv) {
+	const resultsTable = document.createElement("table");
+	resultsTable.className = "table"; // for Bootstrap
+	resultsDiv.replaceChildren(resultsTable); 
+
+	const resultsTHead = document.createElement("thead");
+	resultsTable.appendChild(resultsTHead);
+	const resultsVarTR = document.createElement("tr");
+	resultsTHead.appendChild(resultsVarTR);
+
+	for (var variable of [ "s", "p", "o" ]) 
+	{
+		const resultsVarTH = document.createElement("th");
+		resultsVarTR.appendChild(resultsVarTH);
+		const resultsVarText = document.createTextNode(variable);
+		resultsVarTH.appendChild(resultsVarText);
+	}
+
+	const resultsTBody = document.createElement("tbody");
+	resultsTable.appendChild(resultsTBody);
+
+	resultJson.forEach(function (resource) {
+		const keys = Object.keys(resource);
+
+		keys.filter(key => key != "@id" && key != "@type").forEach((property, index) => {
+			const valuesObjs = resource[property];
+
+			valuesObjs.forEach((valueObj) => {
+				const tripleTR = document.createElement("tr");
+				resultsTBody.appendChild(tripleTR);
+
+				const subjectTD = document.createElement("td");
+				tripleTR.appendChild(subjectTD);
+				const subjectText = document.createTextNode("<" + resource["@id"] + ">");// TO-DO: _:bnode
+				subjectTD.appendChild(subjectText);
+
+				const propertyTD = document.createElement("td");
+				tripleTR.appendChild(propertyTD);
+				const propertyText = document.createTextNode("<" + property + ">");
+				propertyTD.appendChild(propertyText);
+
+				const objectTD = document.createElement("td");
+				tripleTR.appendChild(objectTD);
+
+				var objectString = (valueObj["@value"]) ? "\"" + valueObj["@value"] + "\"" : "<" + valueObj["@id"] + ">"; // TO-DO: _:bnode
+				if (valueObj["@type"]) objectString += "^^" + "<" + valueObj["@type"] + ">";
+				if (valueObj["@language"]) objectString += "@" + valueObj["@language"];
+
+				const objectText = document.createTextNode(objectString);
+				objectTD.appendChild(objectText);
+			});
+		});
+
+		if (resource["@type"])
+		{
+			resource["@type"].forEach((typeObj) => {
+				const tripleTR = document.createElement("tr");
+				resultsTBody.appendChild(tripleTR);
+
+				const subjectTD = document.createElement("td");
+				tripleTR.appendChild(subjectTD);
+				const subjectText = document.createTextNode("<" + resource["@id"] + ">");// TO-DO: _:bnode
+				subjectTD.appendChild(subjectText);
+
+				const propertyTD = document.createElement("td");
+				tripleTR.appendChild(propertyTD);
+				const propertyText = document.createTextNode("<http://www.w3.org/1999/02/22-rdf-syntax-ns#type>");
+				propertyTD.appendChild(propertyText);
+
+				const objectTD = document.createElement("td");
+				tripleTR.appendChild(objectTD);
+
+				var objectString;
+				if (typeof typeObj === 'string') objectString = "<" + typeObj + ">";
+				else
+				{
+					objectString = (typeObj["@value"]) ? "\"" + typeObj["@value"] + "\"" : "<" + typeObj["@id"] + ">"; // TO-DO: _:bnode
+					if (typeObj["@type"]) objectString += "^^" + "<" + typeObj["@type"] + ">";
+					if (typeObj["@language"]) objectString += "@" + typeObj["@language"];
+				}
+
+				const objectText = document.createTextNode(objectString);
+				objectTD.appendChild(objectText);
+			});
+		}
+	});
+}
+
+function renderSPARQLJSON(resultJson, resultsDiv) {
+	const resultsTable = document.createElement("table");
+	resultsTable.className = "table"; // for Bootstrap
+	resultsDiv.replaceChildren(resultsTable); 
+
+	const resultsTHead = document.createElement("thead");
+	resultsTable.appendChild(resultsTHead);
+	const resultsVarTR = document.createElement("tr");
+	resultsTHead.appendChild(resultsVarTR);
+
+	for (var variable of resultJson.head.vars) 
+	{
+		const resultsVarTH = document.createElement("th");
+		resultsVarTR.appendChild(resultsVarTH);
+		const resultsVarText = document.createTextNode(variable);
+		resultsVarTH.appendChild(resultsVarText);
+	}
+
+	const resultsTBody = document.createElement("tbody");
+	resultsTable.appendChild(resultsTBody);
+
+	for (var binding of resultJson.results.bindings)
+	{
+		const resultsBindingTR = document.createElement("tr");
+		resultsTBody.appendChild(resultsBindingTR);
+
+		for (var variable of resultJson.head.vars)
+		{
+			const resultsBindingTD = document.createElement("td");
+			resultsBindingTR.appendChild(resultsBindingTD);
+
+			const value = binding[variable].value;
+			var resultsBindingString = (binding[variable].type == "literal") ? "\"" + value + "\"" : "<" + value + ">";
+			if (binding[variable].datatype) resultsBindingString += "^^" + "<" + binding[variable].datatype + ">";
+			if (binding[variable]["xml:lang"]) resultsBindingString += "@" + binding[variable]["xml:lang"];
+			const resultsBindingText = document.createTextNode(resultsBindingString);
+			resultsBindingTD.appendChild(resultsBindingText);
+		}
+	}
 }
